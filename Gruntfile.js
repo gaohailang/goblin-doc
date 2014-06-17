bc52c3dd21fde7ce1f51dee3330d1624c926a6ed
HailangGao <gaohailang@wandoujia.com>
2014-06-17 10:45:32 +0800

    feat: add a readme.md builder as grunt task from topics folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,6 @@ module.exports = function(grunt) {
                 ' */'
         },
 
-
         watch: {
             markdown: {
                 options: {
@@ -56,7 +55,50 @@ module.exports = function(grunt) {
                 src: ['README.md', '*.md', 'app/*.md', 'topics/*.md'],
                 dest: 'dist/all.md'
             }
-        }
+        },
+    });
+
+    grunt.registerTask('build-readme', 'extract title and summary from topics directory to append to readme.md', function() {
+        // got [{title: '', summary: '', link: ''}]
+        // var done = this.async();
+
+        var distillDict = [];
+        var titleSummaryTemp;
+        grunt.file.recurse('./topics', function(fileSrc) {
+            // grunt.file.recurse('./topics', function(){console.log(arguments)}) to sniffer arguments
+            content = grunt.file.read(fileSrc);
+            titleSummaryTemp = content.split('\n').slice(0, 2);
+            distillDict.push({
+                link: fileSrc,
+                title: titleSummaryTemp[0],
+                summary: titleSummaryTemp[1]
+            });
+        });
+
+        var itemTpl = '[<%= title %>](<%= link %>): <%= summary %>';
+        var allItemStr = '';
+        distillDict.forEach(function(val, idx) {
+            allItemStr += grunt.template.process(itemTpl, {
+                data: val
+            }) + '\n';
+        });
+        console.log(allItemStr);
+
+        // append to readme.md
+        readmeContent = grunt.file.read('readme.md');
+        // console.log(readmeContent.replace(/### Topics\n.*(?=\n\n### TODO)/, '### Topics\n' + allItemStr));
+    });
+
+    grunt.registerTask('asyncfoo', 'My "asyncfoo" task.', function() {
+        // Force task into async mode and grab a handle to the "done" function.
+        var done = this.async();
+        // Run some sync stuff.
+        grunt.log.writeln('Processing task...');
+        // And some async stuff.
+        setTimeout(function() {
+            grunt.log.writeln('All done!');
+            done();
+        }, 2000);
     });
 
     // Dependencies
